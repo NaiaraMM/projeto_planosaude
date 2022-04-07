270388dad74b4d3951863b9d8b013e1f8f9de78f
Naiara de Mello Martins <44578089+NaiaraMM@users.noreply.github.com>
2022-04-07 18:29:29 -0300

    subir projeto

---
+++ projeto_planosaude/src/main/java/com/naiara/projeto_planosaude/ProjetoPlanosaudeApplication.java
@@ -0,0 +1,13 @@
+package com.naiara.projeto_planosaude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoPlanosaudeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoPlanosaudeApplication.class, args);
+	}
+
+}
---
+++ projeto_planosaude/src/test/java/com/naiara/projeto_planosaude/ProjetoPlanosaudeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naiara.projeto_planosaude;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoPlanosaudeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
